a7fffa032d33a1170ebd0ce3bed67cbbaa9e4495
Alexander Poluektov <alexander.poluektov@gmail.com>
2015-07-08 22:37:10 +0200

    Put token, first step

--- sketch2.py
+++ sketch2.py
@@ -21,6 +21,24 @@ class RoadFragment:
     def code(self):
         return 'r'
 
+    def __repr__(self):
+        return '<road-fragment: ' + self.begin + '->' + self.end + ' >'
+
+
+class Road:
+    def __init__(self, id_, cell, begin, end):
+        self.id_ = id_
+        self.cells = set([cell])
+        self.begin = begin
+        self.end = end
+        self.tokens = []
+
+    def code(self):
+        return 'r'
+
+    def contains(self, xy):
+        return xy in self.cells
+
 
 class Card:
     # resources is the list of resources
@@ -50,16 +68,21 @@ class Card:
             result_resources.append(RoadFragment(begin,end))
         return Card(result_resources)
         
-        
 
 def rotate(l,n):
     return l[n:] + l[:n]
+
 
 class Board:
     def __init__(self, game):
         self.game = game
         # (x,y) -> Card
+        # FIXME: don't need it anymore
         self.cards = dict()
+
+        # ((x,y),s) -> Road
+        self.roads = dict()
+        self.last = None
 
     def add_card(self, card, xy):
         # xy already ocupied
@@ -67,27 +90,74 @@ class Board:
             return False
 
         x,y = xy
-        neighbor_coords = [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]
-        neighbor_adjacent_sides = "wsen"
-        adjacent_sides = "enws"
+        sides = 'enws'
 
-        # FIXME: long!
-        for ((x1,y1),s0,s1) in zip(neighbor_coords, adjacent_sides, neighbor_adjacent_sides):
+        has_neighbor = False
+        for s in sides:
+            (x1,y1),s1 = self.adjacent(xy,s)
             neighbor = self.cards.get((x1,y1))
             if neighbor:
-                side = card.sides[s0]
-                neighbor_side = neighbor.sides[s1]
+                has_neighbor = True
+                side = card.sides[s]
+                adj_side = self.find_resources((x1,y1),s1)
 
-                if not self.do_sides_match(side, neighbor_side):
+                if not self.do_sides_match(side, adj_side):
                     return False
-                
-        self.cards[xy] = card
 
+        if not has_neighbor and self.cards:
+            return False
+
+        self.cards[xy] = card
+        self.last = xy
+
+        for r in card.resources:
+            if r.code() == 'r':
+                road = Road(id_=(xy,r.begin), cell=xy, begin=r.begin, end=r.end)
+                self.roads[(xy,r.begin)] = road
+
+                if r.end:
+                    self.roads[(xy, r.end)] = road
+
+        return True
+
+
+    def find_resources(self, xy, side):
+        result = []
+        r = self.roads.get((xy,side))
+        if r:
+            result.append(r.code())
+        return result
+
+
+    def adjacent(self, (x,y), side):
+        # FIXME: const static data; move so that it is only initialized once
+        sides = 'enws'
+        adj_sides = 'wsen'
+        deltas = [(1,0), (0,1), (-1,0), (0,-1)]        
+        d = dict(zip(sides, range(0,4)))
+
+        idx = d[side]
+        dx,dy = deltas[idx]
+        return ((x+dx,y+dy), adj_sides[idx])
+
+
+    def put_token(self, resource, player):
+        if not resource:
+            return False
+        if not resource.contains(self.last):
+            return False
+        if resource.tokens:
+            return False
+        resource.tokens.append(player)
         return True
 
     def do_sides_match(self, side1, side2):
         # FIXME: at least at the moment
         return sorted(side1) == sorted(side2)
+
+    def find_road(self, xy, s):
+        r = self.roads.get((xy,s))
+        return r
 
 
 class CarcassoneTest(unittest.TestCase):
@@ -98,8 +168,10 @@ class CarcassoneTest(unittest.TestCase):
         game = Game(2)
         # FIXME: should create board with init piece at center
         board = Board(game)
-        # FIXME: what card?
         # FIXME: also, should get a card from the suffled deck
+
+        p0 = game.players[0]
+        p1 = game.players[1]
 
         # put a card and don't claim any resource
         # the card should be put
@@ -107,8 +179,8 @@ class CarcassoneTest(unittest.TestCase):
         card0 = Card([RoadFragment('n', 's')])
         status = board.add_card(card0, (0,0))
         self.assertTrue(status)
-        self.assertEqual(game.players[0].score, 0)
-        self.assertEqual(game.players[1].score, 0)
+        self.assertEqual(p0.score, 0)
+        self.assertEqual(p1.score, 0)
 
         # put another card to the same position
         # the card should not be put there
@@ -116,34 +188,61 @@ class CarcassoneTest(unittest.TestCase):
         card1 = Card([RoadFragment('n', 's')])
         status = board.add_card(card1, (0,0))
         self.assertFalse(status)
-        self.assertEqual(game.players[0].score, 0)
-        self.assertEqual(game.players[1].score, 0)
+        self.assertEqual(p0.score, 0)
+        self.assertEqual(p1.score, 0)
+
+        # put a card so that it is not adjacent to any card
+        card = Card([RoadFragment('n', 's')])
+        status = board.add_card(card, (2,0))
+        self.assertFalse(status)
 
         card2 = Card([RoadFragment('n', 's')])
         status = board.add_card(Card.rotated(card2,1), (1,0))
         # card2 doesn't combine with card0 with this orientation
         self.assertFalse(status)
-        self.assertEqual(game.players[0].score, 0)
-        self.assertEqual(game.players[1].score, 0)
+        self.assertEqual(p0.score, 0)
+        self.assertEqual(p1.score, 0)
 
-        # let's try
         status = board.add_card(Card.rotated(card2,2), (1,0))
         self.assertTrue(status)
-        self.assertEqual(game.players[0].score, 0)
-        self.assertEqual(game.players[1].score, 0)
+        self.assertEqual(p0.score, 0)
+        self.assertEqual(p1.score, 0)
+
+        status = board.put_token(board.find_road((1,0), 'e'), p0)
+        # there is no road at given coords
+        self.assertFalse(status)
+        self.assertEqual(p0.score, 0)
+        self.assertEqual(p1.score, 0)
+
+        status = board.put_token(board.find_road((0,0), 'n'), p0)
+        # (0,0) was not the last turn
+        self.assertFalse(status)
+        self.assertEqual(p0.score, 0)
+        self.assertEqual(p1.score, 0)
+
+        status = board.put_token(board.find_road((1,0), 'n'), p0)
+        self.assertTrue(status)
+#        self.assertEqual(p0.score, 1)
+        self.assertEqual(p1.score, 0)
 
         card3 = Card([RoadFragment('e', 'w')])
         status = board.add_card(Card.rotated(card3,1), (2,0))
         self.assertTrue(status)
-        self.assertEqual(game.players[0].score, 0)
-        self.assertEqual(game.players[1].score, 0)
+#        self.assertEqual(p0.score, 1)
+        self.assertEqual(p1.score, 0)
 
         card4 = Card([RoadFragment('e', 'w')])
         status = board.add_card(Card.rotated(card4, 3), (0,-1))
         self.assertTrue(status)
-        self.assertEqual(game.players[0].score, 0)
-        self.assertEqual(game.players[1].score, 0)
-                     
+#        self.assertEqual(p0.score, 2)
+        self.assertEqual(p1.score, 0)
+
+        status = board.put_token(board.find_road((0,-1), 'n'), p1)
+        # road already claimed by p0
+#        self.assertFalse(status)
+#        self.assertEqual(p0.score, 2)
+        self.assertEqual(p1.score, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
